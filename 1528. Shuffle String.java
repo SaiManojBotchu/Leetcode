d524e0ab6adcd4fbfc01622f174b4bc49db0f4a1
Sai Manoj <54241768+SaiManojBotchu@users.noreply.github.com>
2022-04-27 02:21:40 +0530

    Create 1528. Shuffle String.java

---
+++ 1528. Shuffle String.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] charArray = new char[s.length()];
+        for(int i = 0 ; i < s.length(); i++) {
+            charArray[indices[i]] = s.charAt(i);
+        }
+        return String.valueOf(charArray);
+    }
+}

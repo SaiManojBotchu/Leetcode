67a5237255d3593d82a6484206787e47c5cd65af
Sai Manoj <54241768+SaiManojBotchu@users.noreply.github.com>
2022-04-29 00:29:12 +0530

    Create 520. Detect Capital.java

---
+++ 520. Detect Capital.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length() == 1) return true;
+        
+        int count1 = 0, count2 = 0;
+        
+        for(int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) <= 'Z') count1++;
+            else if (word.charAt(i) >= 'a') count2++;
+        }
+        if (count1 == word.length()) return true;
+        else if (count2 == word.length()) return true;
+        else if (count2 == word.length() - 1 && word.charAt(0) <= 90) return true;
+        else return false;
+    }
+}
+
+// class Solution {
+//     public boolean detectCapitalUse(String word) {
+//         return word.equals(word.toUpperCase()) || word.equals(word.charAt(0) + word.substring(1).toLowerCase());
+//     }
+// }

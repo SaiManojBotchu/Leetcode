4a2ca0b6da29be94494e73e80d0c38fdcc84fb61
Sai Manoj <54241768+SaiManojBotchu@users.noreply.github.com>
2022-05-11 20:25:11 +0530

    Create 31. Next Permutation.java

---
+++ Striver/Arrays/31. Next Permutation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void nextPermutation(int[] A) {
+        if(A.length <= 1) return;
+        
+        int i = A.length - 2;
+        while (i >= 0 && A[i] >= A[i + 1]) i--;
+        if (i >= 0) {
+            int j = A.length - 1;
+            while (j >= 0 && A[j] <= A[i]) j--;
+            swap(A, i, j);
+        }
+        reverse(A, i + 1, A.length - 1);
+    }
+    public void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+    public void reverse(int[] A, int i, int j) {
+        while (i < j) swap(A, i++, j--);
+    }
+}

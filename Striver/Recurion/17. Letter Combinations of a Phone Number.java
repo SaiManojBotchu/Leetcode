2fca175fc32653a3f1002c3ac1afa3bc119cbf88
Sai Manoj <54241768+SaiManojBotchu@users.noreply.github.com>
2022-05-26 13:47:08 +0530

    Update 17. Letter Combinations of a Phone Number.java

--- Striver/Recurion/17. Letter Combinations of a Phone Number.java
+++ Striver/Recurion/17. Letter Combinations of a Phone Number.java
@@ -1,26 +1,54 @@
 class Solution {
     List<String> res = new ArrayList<>();
     String[] map = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-    char[] c;
     
     public List<String> letterCombinations(String s) {
         if (s.isEmpty()) return res;
         
-        c = s.toCharArray();
-        backtrack(0, "");
+        backtrack(0, s.toCharArray(), new StringBuilder());
         return res;
     }
     
-    public void backtrack(int start, String s) {
+    public void backtrack(int start, char[] c, StringBuilder sb) {
         if (start == c.length) {
-            res.add(s);
+            res.add(sb.toString());
             return;
         }
+        
         int digit = c[start] - '0';
         String val = map[digit];
-        
         for (int i = 0; i < val.length(); i++) {
-            backtrack(start + 1, s + val.charAt(i));
+            sb.append(val.charAt(i));
+            backtrack(start + 1, c, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
+
+// Solution 2 (more TC - 9ms)
+// class Solution {
+//     List<String> res = new ArrayList<>();
+//     String[] map = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+//     char[] c;
+    
+//     public List<String> letterCombinations(String s) {
+//         if (s.isEmpty()) return res;
+        
+//         c = s.toCharArray();
+//         backtrack(0, "");
+//         return res;
+//     }
+    
+//     public void backtrack(int start, String s) {
+//         if (start == c.length) {
+//             res.add(s);
+//             return;
+//         }
+//         int digit = c[start] - '0';
+//         String val = map[digit];
+        
+//         for (int i = 0; i < val.length(); i++) {
+//             backtrack(start + 1, s + val.charAt(i));
+//         }
+//     }
+// }

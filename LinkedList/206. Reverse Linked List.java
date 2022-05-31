88619e7cae360c23fc8a6b9733e65a50a4673117
Sai Manoj <54241768+SaiManojBotchu@users.noreply.github.com>
2022-05-31 11:46:17 +0530

    Update 206. Reverse Linked List.java

--- LinkedList/206. Reverse Linked List.java
+++ LinkedList/206. Reverse Linked List.java
@@ -1,12 +1,28 @@
+// Iterative
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode newHead = null, next = null;
+        ListNode newHead = null, nextNode = null;
         while (head != null) {
-            next = head.next;
+            nextNode = head.next;
             head.next = newHead;
             newHead = head;
-            head = next;
+            head = nextNode;
         }
         return newHead;
     }
 }
+
+// Recursive
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         return reverseLL(head, null);
+//     }
+//     public ListNode reverseLL(ListNode head, ListNode newHead) {
+//         if (head == null) {
+//             return newHead;
+//         }
+//         ListNode nextNode = head.next;
+//         head.next = newHead;
+//         return reverseLL(nextNode, head);
+//     }
+// }

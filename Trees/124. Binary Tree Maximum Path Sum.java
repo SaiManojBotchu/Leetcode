9dabd3cdeced50c474badfc225cab892a42b8925
Sai Manoj <54241768+SaiManojBotchu@users.noreply.github.com>
2022-06-15 21:28:57 +0530

    Update 124. Binary Tree Maximum Path Sum.java

--- Trees/124. Binary Tree Maximum Path Sum.java
+++ Trees/124. Binary Tree Maximum Path Sum.java
@@ -8,6 +8,8 @@ class Solution {
     public int recursion(TreeNode node) {
         if (node == null) return 0;
         
+        // since the nodes has chance of negative values we are only taking 
+        // the node if its value is greater than 0 else dont consider the node
         int maxL = Math.max(0, recursion(node.left));
         int maxR = Math.max(0, recursion(node.right));
         maxPathSum = Math.max(maxPathSum, node.val + maxL + maxR);

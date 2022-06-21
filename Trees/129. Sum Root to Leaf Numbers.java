5739760b453237e1d51b04be71f597ffb44f0530
Sai Manoj <54241768+SaiManojBotchu@users.noreply.github.com>
2022-06-21 22:23:57 +0530

    Create 129. Sum Root to Leaf Numbers.java

---
+++ Trees/129. Sum Root to Leaf Numbers.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return helper(root, 0);
+    }
+    public int helper(TreeNode node, int sum) {
+        if (node == null) return 0;
+        
+        sum = sum * 10 + node.val;
+        if (node.left == null && node.right == null) {
+            return sum;
+        }
+        return helper(node.left, sum) + helper(node.right, sum);
+    }
+}

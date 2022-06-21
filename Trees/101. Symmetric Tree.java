389d8a384b3a3440746e83572b793a3f55ba6dd4
Sai Manoj <54241768+SaiManojBotchu@users.noreply.github.com>
2022-06-21 21:42:20 +0530

    Create 101. Symmetric Tree.java

---
+++ Trees/101. Symmetric Tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root.left, root.right);
+    }
+    
+    public boolean helper(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+}
